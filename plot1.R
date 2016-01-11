361a887e19214ef71f70dd416661d381a7dd8002
mazendiab <mazendiab@gmail.com>
2016-01-11 00:04:01 -0600

    2nd commit

--- plot1.R
+++ plot1.R
@@ -1,29 +1,29 @@
+# This codes generate plot1 in this assignment
 rm(list=ls())
-# required packages
+# load necessary packages
 library(data.table)
 library(lubridate)
 
-# download the zip file and unzip
+# download the zipped file for the dataset  then unzip the file
 file.url<-'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
 download.file(file.url,destfile='power_consumption.zip')
-unzip('power_consumption.zip',exdir='source data',overwrite=TRUE)
-# read the raw table and limit to 2 days
-variable.class<-c(rep('character',2),rep('numeric',7))
-power.consumption<-read.table('household_power_consumption.txt',header=TRUE,sep=';',na.strings='?',colClasses=variable.class)
-power.consumption<-power.consumption[power.consumption$Date=='1/2/2007' | power.consumption$Date=='2/2/2007',]
+unzip('power_consumption.zip',overwrite=TRUE)
+
+# read data from the dates 2007-02-01 and 2007-02-02
+Classes<-c(rep('character',2),rep('numeric',7))
+Electricpower.consumption<-read.table('household_power_consumption.txt',header=TRUE,sep=';',na.strings='?',colClasses=Classes)
+Electricpower.consumption<- Electricpower.consumption[Electricpower.consumption$Date=='1/2/2007' | Electricpower.consumption$Date=='2/2/2007',]
   
- # clean up the variable names and convert date/time fields
-  cols<-c('Date','Time','GlobalActivePower','GlobalReactivePower','Voltage','GlobalIntensity','SubMetering1','SubMetering2','SubMetering3')
-  colnames(power.consumption)<-cols
-  power.consumption$DateTime<-dmy(power.consumption$Date)+hms(power.consumption$Time)
-  power.consumption<-power.consumption[,c(10,3:9)]
+Cols<-c('Date','Time','GlobalActivePower','GlobalReactivePower','Voltage','GlobalIntensity','SubMetering1','SubMetering2','SubMetering3')
+colnames(Electricpower.consumption)<-Cols
+Electricpower.consumption$DateTime<-dmy(Electricpower.consumption$Date)+hms(Electricpower.consumption$Time)
+Electricpower.consumption<-Electricpower.consumption[,c(10,3:9)]
   
- 
 # open device
 png(filename='plot1.png',width=480,height=480,units='px')
 
 # plot data
-hist(power.consumption$GlobalActivePower,main='Global Active Power',xlab='Global Active Power (kilowatts)',col='red')
+hist(Electricpower.consumption$GlobalActivePower,main='Global Active Power',xlab='Global Active Power (kilowatts)',col='red')
 
 # Turn off device
 x<-dev.off()
--- plot4.R
+++ plot4.R
@@ -1,22 +1,23 @@
+# This codes generate plot1 in this assignment
 rm(list=ls())
-# required packages
+# load necessary packages
 library(data.table)
 library(lubridate)
 
-# download the zip file and unzip
+# download the zipped file for the dataset  then unzip the file
 file.url<-'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
 download.file(file.url,destfile='power_consumption.zip')
-unzip('power_consumption.zip',exdir='source data',overwrite=TRUE)
-# read the raw table and limit to 2 days
-variable.class<-c(rep('character',2),rep('numeric',7))
-power.consumption<-read.table('household_power_consumption.txt',header=TRUE,sep=';',na.strings='?',colClasses=variable.class)
-power.consumption<-power.consumption[power.consumption$Date=='1/2/2007' | power.consumption$Date=='2/2/2007',]
+unzip('power_consumption.zip',overwrite=TRUE)
 
-# clean up the variable names and convert date/time fields
-cols<-c('Date','Time','GlobalActivePower','GlobalReactivePower','Voltage','GlobalIntensity','SubMetering1','SubMetering2','SubMetering3')
-colnames(power.consumption)<-cols
-power.consumption$DateTime<-dmy(power.consumption$Date)+hms(power.consumption$Time)
-power.consumption<-power.consumption[,c(10,3:9)]
+# read data from the dates 2007-02-01 and 2007-02-02
+Classes<-c(rep('character',2),rep('numeric',7))
+Electricpower.consumption<-read.table('household_power_consumption.txt',header=TRUE,sep=';',na.strings='?',colClasses=Classes)
+Electricpower.consumption<- Electricpower.consumption[Electricpower.consumption$Date=='1/2/2007' | Electricpower.consumption$Date=='2/2/2007',]
+
+Cols<-c('Date','Time','GlobalActivePower','GlobalReactivePower','Voltage','GlobalIntensity','SubMetering1','SubMetering2','SubMetering3')
+colnames(Electricpower.consumption)<-Cols
+Electricpower.consumption$DateTime<-dmy(Electricpower.consumption$Date)+hms(Electricpower.consumption$Time)
+Electricpower.consumption<-Electricpower.consumption[,c(10,3:9)]
 
 # open device
 png(filename='plot4.png',width=480,height=480,units='px')
@@ -25,20 +26,20 @@ png(filename='plot4.png',width=480,height=480,units='px')
 par(mfrow=c(2,2))
 
 # plot data on top left (1,1)
-plot(power.consumption$DateTime,power.consumption$GlobalActivePower,ylab='Global Active Power',xlab='',type='l')
+plot(Electricpower.consumption$DateTime,Electricpower.consumption$GlobalActivePower,ylab='Global Active Power',xlab='',type='l')
 
 # plot data on top right (1,2)
-plot(power.consumption$DateTime,power.consumption$Voltage,xlab='datetime',ylab='Voltage',type='l')
+plot(Electricpower.consumption$DateTime,Electricpower.consumption$Voltage,xlab='datetime',ylab='Voltage',type='l')
 
 # plot data on bottom left (2,1)
 lncol<-c('black','red','blue')
 lbls<-c('Sub_metering_1','Sub_metering_2','Sub_metering_3')
-plot(power.consumption$DateTime,power.consumption$SubMetering1,type='l',col=lncol[1],xlab='',ylab='Energy sub metering')
-lines(power.consumption$DateTime,power.consumption$SubMetering2,col=lncol[2])
-lines(power.consumption$DateTime,power.consumption$SubMetering3,col=lncol[3])
+plot(Electricpower.consumption$DateTime,Electricpower.consumption$SubMetering1,type='l',col=lncol[1],xlab='',ylab='Energy sub metering')
+lines(Electricpower.consumption$DateTime,Electricpower.consumption$SubMetering2,col=lncol[2])
+lines(Electricpower.consumption$DateTime,Electricpower.consumption$SubMetering3,col=lncol[3])
 
 # plot data on bottom right (2,2)
-plot(power.consumption$DateTime,power.consumption$GlobalReactivePower,xlab='datetime',ylab='Global_reactive_power',type='l')
+plot(Electricpower.consumption$DateTime,Electricpower.consumption$GlobalReactivePower,xlab='datetime',ylab='Global_reactive_power',type='l')
 
 # close device
 x<-dev.off()
